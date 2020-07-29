92ed7fb54c808b56a8cfc47702b776126d8c0230
AlexandrPasko <pasko2050@gmail.com>
2020-07-29 18:45:44 -0500

    route to homepage added

--- watches/routes/web.php
+++ watches/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+//Route::get('/', 'WatchesController@home');
