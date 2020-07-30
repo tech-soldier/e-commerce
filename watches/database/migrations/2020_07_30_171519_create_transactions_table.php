fe99ff0a4bd9d72df2d491f82a161d44b4a37777
Aina Hazel <ainahazel@Ainas-MBP.hitronhub.home>
2020-07-30 12:17:31 -0500

    aina -- made the transaction table migration file

---
+++ watches/database/migrations/2020_07_30_171519_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id('transaction_id');
+            $table->integer('order_id')->unsigned(); 
+            $table->string('transaction_code', 255); 
+            $table->text('transaction'); 
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
