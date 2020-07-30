1c1f443a97db8e91012a3834957df5d084ef371e
Aina Hazel <ainahazel@Ainas-MBP.hitronhub.home>
2020-07-30 12:09:45 -0500

    aina -- just made a comment

--- watches/database/migrations/2020_07_30_170441_create_customers_table.php
+++ watches/database/migrations/2020_07_30_170441_create_customers_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateCustomersTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations for the customers table 
      *
      * @return void
      */
