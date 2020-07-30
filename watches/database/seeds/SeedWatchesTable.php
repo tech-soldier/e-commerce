e0361efa2caea74c80f092a079f9b26dc3b5f950
Aina Hazel <ainahazel@Ainas-MBP.hitronhub.home>
2020-07-30 13:19:01 -0500

    aina -- seeded the categories table

---
+++ watches/database/seeds/SeedWatchesTable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SeedWatchesTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('watches')->insert([
+        // 	'watch_id' => 1, 
+        // 	'watch_name' => 'Bamity', 
+        // 	'in_stock' => 1, 
+        // 	'quantity' => 26, 
+        // 	'price' => 1399.99, 
+        // 	'cost' => 1799.99, 
+        // 	'material' => 'gold', 
+        // 	'main_color' => 'gold', 
+        // 	'movement' => 'quartz', 
+        // 	'gender' => 'male', 
+        // 	'category_id' => 1, 
+        // 	'diameter' => '33', 
+
+
+
+
+        ]); 
+    }
+}
