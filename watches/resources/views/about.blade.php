d6975d2e3b4568cdc799571d90a267211daf13c7
DawnBaker <deltabravo73@gmail.ca>
2020-07-30 18:29:58 -0500

    updated a couple lines on the about page, and changes some styling for the paragraphs, margin-top and lineheight

--- watches/resources/views/about.blade.php
+++ watches/resources/views/about.blade.php
@@ -35,7 +35,7 @@
     
 
     <!-- Best Selling Products -->
-    <section class="about_us">
+    <section class="about-us-area mb-4">
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -57,11 +57,10 @@
                 <div class="col-6">
                     <div class="about_txt">
                         <div class="about_txt">
-                            <p>TechWatch is a premiere maker of watches for today's world of tech and style. We are the go to brand for for young business professionals in these busy and often hectic times. 
-                            TechWatch was founded in 2005 and quickly gained popularity in the urban youth population when we launched our first online store in 2015. Since then, it has carved a niche for itself with watches that are fashionable and affordable.
-                            We have a wide range of products from sports watches, to formal watches.
-                            Today, our brand has successfully gained the title of the most loved youth fashion brand in the country
-                            We offer hassle-free online shopping for our customers throughout Canada.  Take a minute to check out our store and see what we have for you!</p>
+                            <p class="font-weight-bold">TechWatch is a premiere maker of watches for today's world of tech and style. We are the go to brand for for young business professionals in these busy and often hectic times. Today, our brand has successfully gained the title of the most loved youth fashion brand in the country</p>
+                            <p class="font-weight-bold">TechWatch was founded in 2005 and quickly gained popularity in the urban youth population when we launched our first online store in 2015. Since then, it has carved a niche for itself with watches that are fashionable and affordable.</p>
+                            <p class="font-weight-bold">We have a wide range of products from sports watches, to formal watches.  There are plenty of styles for women and men and watch band sizes from narrow to thick, in many different colors to suit your style, whatever that may be!</p>
+                            <p class="font-weight-bold">We offer hassle-free online shopping for our customers throughout Canada.  Take a minute to check out our store and see what we have for you!</p>
                         </div>
                     </div>
                     
