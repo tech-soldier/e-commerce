03d705f8efaaefebb954e1832e98b80b15523d79
Aina Hazel <ainahazel@Ainas-MBP.hitronhub.home>
2020-07-30 16:55:37 -0500

    aina -- protected fillable in customers model

--- watches/app/Customer.php
+++ watches/app/Customer.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+
+	protected $fillable = [
+		'customer_id',
+		'first_name', 
+		'last_name', 
+		'billing_address', 
+		'city', 
+		'province',
+		'country', 
+		'postal_code',
+		'email_address', 
+		'phone_number', 
+		'password' 
+	]; 
+
     public function order()
     {
     	return $this->hasMany(Order::class); 
