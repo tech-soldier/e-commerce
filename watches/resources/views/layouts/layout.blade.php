09463821d0ec10be01810e38deda2e6d3dfc2665
AlexandrPasko <pasko2050@gmail.com>
2020-07-30 11:56:23 -0500

    footer is updated

--- watches/resources/views/layouts/layout.blade.php
+++ watches/resources/views/layouts/layout.blade.php
@@ -31,7 +31,7 @@
         <div class="container">
 		  <div class="row">
 		    <div class="col">
-		      <a href="/" class="nav-brand"><img class="logo my-2" src="/images/logo.png" alt="logo"></a>
+		      <a href="/" class="nav-brand"><img class="logo my-3" src="/images/logo.png" alt="logo"></a>
 		    </div>
 		    <div class="col-6">
 		       <!-- Menu -->
