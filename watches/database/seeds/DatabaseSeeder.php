c5bda88542ee134ef3ac89c17dd86754c6129914
Aina Hazel <ainahazel@Ainas-MBP.hitronhub.home>
2020-07-30 16:04:59 -0500

    aina -- made seeder for admin and later will fill seeder for customers

--- watches/database/seeds/DatabaseSeeder.php
+++ watches/database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(SeedCategoriesTable::class);
         $this->call(SeedWatchesTable::class);
+        $this->call(SeedAdminsTable::class);
     }
 }
