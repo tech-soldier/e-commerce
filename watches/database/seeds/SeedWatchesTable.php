fca38cf78f7bcaf91edcd9a55522be34ef8ad6d7
Aina Hazel <ainahazel@Ainas-MBP.hitronhub.home>
2020-07-31 06:46:58 -0500

    aina -- 15/50 records for watches

--- watches/database/seeds/SeedWatchesTable.php
+++ watches/database/seeds/SeedWatchesTable.php
@@ -262,9 +262,127 @@ class SeedWatchesTable extends Seeder
             'updated_at' => Carbon::now()
         ]); 
 
+        // next 5 will be all material: silicone rubber 
+        DB::table('watches')->insert([ 
+            'SKU' => 23561980,
+            'watch_name' => 'Asoka', 
+            'in_stock' => 1, 
+            'quantity' => 1222, 
+            'price' => 219.99, 
+            'cost' => 411.99, 
+            'material' => 'silicone rubber', 
+            'main_color' => 'blue', 
+            'movement' => 'automatic', 
+            'gender' => 'male', 
+            'category_id' => 1, 
+            'diameter' => 28,
+            'strap_length' => '195mm', 
+            'strap_width' => '25mm', 
+            'weight' => '15g', 
+            'water_resistant' => '19 atm', 
+            'cover_img' => 'asoka.jpg', 
+            'short_description' => 'The Asoka watch bracelet refined and upgraded.', 
+            'long_description' => 'The Asoka model conveys sporty and elegant at the same time while providing waterproof for over 19 meters below water. Our latest watch made of silicone rubber to provide a lightweight and practical everyday watch', 
+            'created_at' => Carbon::now(), 
+            'updated_at' => Carbon::now()
+        ]); 
 
+        DB::table('watches')->insert([ 
+            'SKU' => 20361980,
+            'watch_name' => 'Opela', 
+            'in_stock' => 1, 
+            'quantity' => 92, 
+            'price' => 211.99, 
+            'cost' => 421.99, 
+            'material' => 'silicone rubber', 
+            'main_color' => 'white', 
+            'movement' => 'automatic', 
+            'gender' => 'male', 
+            'category_id' => 1, 
+            'diameter' => 28,
+            'strap_length' => '185mm', 
+            'strap_width' => '24mm', 
+            'weight' => '16g', 
+            'water_resistant' => '19 atm', 
+            'cover_img' => 'opela.jpg', 
+            'short_description' => 'The Opela 2020 series introduces a blend of critical and robust complexity and simplicity.', 
+            'long_description' => 'The Opela model features a smart feature that allows for phone calls, music, and more. Our latest watch made of silicone rubber to provide a lightweight and practical everyday watch', 
+            'created_at' => Carbon::now(), 
+            'updated_at' => Carbon::now()
+        ]); 
 
-        // next 5 will be all maerial: silicone rubber 
+        DB::table('watches')->insert([ 
+            'SKU' => 20001980,
+            'watch_name' => 'Wrapsafe', 
+            'in_stock' => 1, 
+            'quantity' => 9, 
+            'price' => 249.99, 
+            'cost' => 449.99, 
+            'material' => 'silicone rubber', 
+            'main_color' => 'black', 
+            'movement' => 'mechanical', 
+            'gender' => 'female', 
+            'category_id' => 1, 
+            'diameter' => 25,
+            'strap_length' => '165mm', 
+            'strap_width' => '20mm', 
+            'weight' => '19g', 
+            'water_resistant' => '22 atm', 
+            'cover_img' => 'wrapsafe.jpg', 
+            'short_description' => 'The Wrapsafe 2020 series provides comfort and security.', 
+            'long_description' => 'The Wrapsafe model is meant for outdoor activities, meant for everyday use, meant for it to be tossed and turned and bodied. Through and through, although the Wrapsafe is lightweight, do not let it fool you into thinking it won\'t last.  Our second latest watch made of silicone rubber to provide a lightweight and practical everyday watch', 
+            'created_at' => Carbon::now(), 
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('watches')->insert([ 
+            'SKU' => 20001985,
+            'watch_name' => 'Bitwold', 
+            'in_stock' => 1, 
+            'quantity' => 9, 
+            'price' => 1249.99, 
+            'cost' => 1449.99, 
+            'material' => 'silicone rubber', 
+            'main_color' => 'pink', 
+            'movement' => 'mechanical', 
+            'gender' => 'female', 
+            'category_id' => 1, 
+            'diameter' => 25,
+            'strap_length' => '165mm', 
+            'strap_width' => '21mm', 
+            'weight' => '29g', 
+            'water_resistant' => '30 atm', 
+            'cover_img' => 'bitwold.jpg', 
+            'short_description' => 'Hight quality silicone Material, very soft and durable enough to avoid the slipping-off problem,sweat-resistant and waterproof,Perfect fit for sport wear.', 
+            'long_description' => 'The Bitwold model is meant for outdoor activities, meant for everyday use, meant for it to be tossed and turned and bodied. Through and through, although the Wrapsafe is lightweight, do not let it fool you into thinking it won\'t last.  Our third latest watch made of silicone rubber to provide a lightweight and practical everyday watch', 
+            'created_at' => Carbon::now(), 
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('watches')->insert([ 
+            'SKU' => 20001911,
+            'watch_name' => 'Tresom', 
+            'in_stock' => 1, 
+            'quantity' => 771, 
+            'price' => 299.99, 
+            'cost' => 449.99, 
+            'material' => 'silicone rubber', 
+            'main_color' => 'white', 
+            'movement' => 'automatic', 
+            'gender' => 'male', 
+            'category_id' => 1, 
+            'diameter' => 25,
+            'strap_length' => '199mm', 
+            'strap_width' => '25mm', 
+            'weight' => '29g', 
+            'water_resistant' => '30 atm', 
+            'cover_img' => 'tresom.jpg', 
+            'short_description' => 'This is a genuine TechWatch Tresom silicone rubber watch strap.', 
+            'long_description' => 'POLISHED AND SATIN-FINISHED LIGHT BLUE automatic Tresom. TechWatch\'s strength resides in its very clear identity - the art of fusion - this unique ability to create timepieces which combine tradition and innovation', 
+            'created_at' => Carbon::now(), 
+            'updated_at' => Carbon::now()
+        ]); 
+
 
         // next 5 will be all material: silver 
 
