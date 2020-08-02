a2602ee557f54eb3a9f484cab03372aa5b6ac303
DawnBaker <deltabravo73@gmail.ca>
2020-08-02 15:09:14 -0500

    contact.blade.php updated with new html, but images not showing yet, 2 images, the main page image and the map image

--- watches/resources/views/contact.blade.php
+++ watches/resources/views/contact.blade.php
@@ -2,85 +2,68 @@
 
 @section('content')
 
-<div class="container my-5">
-
-    <style>
-    .border-top {
-      border-top: 5px solid #33b5e5 !important;
-      border-top-left-radius: .25rem!important;
-        border-top-right-radius: .25rem!important;
-    }
-  </style>
-    
-
-
-  <div class="top-img">
-      <img src="/images/contact_banner.jpg" alt="">
-  </div>
-  <!--Section: Content-->
-  <section class="text-center dark-grey-text ">
-
-    <div class="card">
-      <div class="card-body rounded-top border-top p-5">
-        
-        <!-- Section heading -->
-        <h3 class="font-weight-bold my-4">Contact Us</h3>
-        <!-- Section description -->
-      
-
-        <form class="mb-4 mx-md-5" action="">
-
-          <div class="row">
-            <div class="col-md-6 mb-4">
-
-              <!-- Name -->
-              <input type="text" id="name" class="form-control" placeholder="Name">
-
-            </div>
-            <div class="col-md-6 mb-4">
-
-              <!-- Email -->
-              <input type="email" id="email" class="form-control" placeholder="Email">
-
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-12 mb-4">
-
-              <!-- Subject -->
-              <input type="text" id="subject" class="form-control" placeholder="Subject">
-
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-12">
-
-              <div class="form-group mb-4">
-                <textarea class="form-control rounded" id="message" rows="3" placeholder="How can we help?"></textarea>
-              </div>
-
-              <div class="text-center">
-                <button type="submit" class="btn btn-info btn-md">Submit</button>
-              </div>
-
-            </div>
-          </div>
-
-        </form>
-        
-      </div>
+<div class="contact1" >
+        <div class="video-container">           
+            <button class="shop-btn text-center"> CONTACT US </button>
+        </div>
     </div>
 
-  </section>
-  <!--Section: Content-->
-    <div class="btm-img">
-      <img src="/images/contact_image.jpg" alt="">
-  </div>
+    <div class="row">
+        <!-- Map -->
+        <div class="col-5 map">                 
+          
+        </div>
 
-</div>
+        <!-- Contact us form -->
+        <div class="col-7">  
+            <!-- content section  -->
+            <section class="text-center dark-grey-text ">
+                <div class="card">
+                    <div class="card-body  p-5">
 
+                        <!-- Section heading -->
+                        <h3 class="font-weight-bold my-4">Contact Us</h3>
+                        <!-- Section description -->
+                        <form class="mb-4 mx-md-5" action="/">
+                            <!-- Contact Info -->
+                            <div class="row">
+                                <div class="col-md-6 mb-4">
+                                    <!-- Name -->
+                                    <input type="text" id="name" class="form-control" placeholder="Name">
+                                </div>
+                                <div class="col-md-6 mb-4">
+                                    <!-- Email -->
+                                    <input type="email" id="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <!-- Subject -->
+                            <div class="row">
+                                <div class="col-md-12 mb-4">
+                                    <input type="text" id="subject" class="form-control" placeholder="Subject">
+                                </div>
+                            </div>
+                            <!-- textarea -->
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group mb-4">
+                                        <textarea class="form-control rounded" id="message" rows="3" placeholder="How can we help?"></textarea>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-info btn-md">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form> <!-- /. form -->
 
+                    </div> <!-- /. card body -->
+                </div> <!-- /. card -->
+            </section> <!-- /. content section  -->
+
+        </div> <!-- /. col -->
+    </div> <!-- /. row -->
+  
+    <!-- <div class="contact2" >
+        This has a image background in css
+    </div> -->
 
 @stop
