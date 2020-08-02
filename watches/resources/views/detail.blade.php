2021312f266a9f98155f9527dcbb6906d665ae3c
DawnBaker <deltabravo73@gmail.ca>
2020-08-02 15:11:59 -0500

    updated detail.blade.php, top image not showing need to figure out why?

--- watches/resources/views/detail.blade.php
+++ watches/resources/views/detail.blade.php
@@ -2,87 +2,122 @@
 
 @section('content')
 
-    <!-- About Slider -->
-    <section class="welcome_area">
-        <!-- <div class="owl-carousel"> -->
+<div class="detail1" >
+        <div class="video-container">                   
+            <button class="shop-btn text-center"> WATCH NAME </button>
+        </div>
+    </div>  <!--/. Header Area  -->
+   
+    <div class="row">
+        <!-- image  -->
+        <div class="col-5  col-md-6 col-lg-6">
+            <img  src="images/product1.jpg" alt="">
+        </div>
 
-        <!-- Single Slide -->
-        <div class="container">
-            <div class="single_slide home-3 bg-img" style="background-image: url(/images/shop_detail_image.jpg);">
-                <!-- <div class="container h-100">
-                    <div class="row h-100 align-items-center">
-                        <div class="col-12 p-0">
-                            <div class="welcome_slide_text text-center">
+        <!-- description -->
+        <div class="col-7  mt-5 col-md-6 col-lg-6">
+            <h3>Rolex Model III</h3>
+            <p class="small">With leather strap</p>
+            <p>$2,999.00</p>
+            <button class="addtobag ">ADD TO BAG</button>
+            <h5 class="mt-5">Product Details</h5>
+            <p class="mt-3">Sku: BQ2249</p>
+            <p class="mt-3">Case Size: 44mm</p>
+            <p class="mt-3">Movement: Quartz Multifunction</p>
+            <p class="mt-3">Platform: Sullivian</p>
+            <p class="mt-3">Strap Material: Leather</p>
+            <p class="mt-3">Water Resistance: 5 ATM</p>
+           
+            <div class="accordion md-accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
+                <!-- Accordion card -->
+                <div class="card">
+                    <!-- Card header -->
+                    <div class="card-header" role="tab" id="headingOne1">
+                      <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true" aria-controls="collapseOne1">
+                        <h5 class="mb-0">
+                          Description
+                          <i class="fas fa-angle-down rotate-icon" aria-hidden="true"></i>
+                        </h5>
+                      </a>
+                    </div> <!-- /. card header -->
 
-                                <div class="bg_blue p-4">
+                    <!-- Card body -->
+                    <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
+                      <div class="card-body">
+                        <p class="mt-4">Sullivian introduced the first Fifty Fathoms watch back in 1953 as the first modern driver's watch and this iconic version , water resitant at upto 1000 feet offer an uodate on the classic with an added date marker and luminescebt hands.</p>
+                      </div>
+                    </div> <!-- /. Card body -->
+                </div> <!-- /. Accordion card -->
 
-                                    <p class="mb-0" data-animation="fadeInUp" data-delay="100ms">Watch Collection</p>
-                                    <i class="fas fa-angle-right"></i>
+                <!-- Accordion card -->
+                <div class="card">
+                    <!-- Card header -->
+                    <div class="card-header" role="tab" id="headingTwo2">
+                        <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2" aria-expanded="false" aria-controls="collapseTwo2">
+                            <h5 class="mb-0">
+                                Details
+                                <i class="fas fa-angle-down rotate-icon" aria-hidden="true"></i>
+                            </h5>
+                        </a>
+                    </div>
+
+                    <!-- Card body -->
+                    <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                        <div class="card-body">
+                            <div class="tech-specs__pivot-menus">
+                                <div class="tech-specs__menu-header">
+                                    <h6 style="font-weight:bold !important;"><strong>Genre</strong></h6>
                                 </div>
-                                <div class="clear"></div>
+                                <ul>
+                                    <li class="tech-specs__menu-items" style="direction:ltr">Racing</li> 
+                                </ul>
+                                <div class="tech-specs__menu-header">
+                                    <h6 style="font-weight:bold !important;"><strong>Audio</strong></h6>
+                                </div>
+                                <ul>
+                                    <li class="tech-specs__menu-item-name">Gran Turismo 79</li>
+                                    <li class="tech-specs__menu-items">Spanish (Latin America)</li>
+                                    <li class="tech-specs__menu-items">English</li>
+                                    <li class="tech-specs__menu-items">Portuguese (Brazil)</li>
+                                    <li class="tech-specs__menu-items">French</li>
+                                </ul>
+                                <div class="tech-specs__menu-header">Subtitles</div>
+                                <ul>
+                                    <li class="tech-specs__menu-item-name">The Art of The Gran Turismo 79</li>
+                                    <li class="tech-specs__menu-items">Spanish (Latin America)</li>
+                                    <li class="tech-specs__menu-items">English</li>
+                                    <li class="tech-specs__menu-items">Portuguese (Brazil)</li>
+                                    <li class="tech-specs__menu-items">French</li>
+                                </ul>
+                                <ul>
+                                    <li class="tech-specs__menu-item-name">The Gran Turismo 79 Original Soundtrack</li>
+                                    <li class="tech-specs__menu-items">Spanish (Latin America)</li>
+                                    <li class="tech-specs__menu-items">English</li>
+                                    <li class="tech-specs__menu-items">Portuguese (Brazil)</li>
+                                    <li class="tech-specs__menu-items">French</li>
+                                </ul>
+                                <ul>
+                                    <li class="tech-specs__menu-item-name">The Gran Turismo 79</li>
+                                    <li class="tech-specs__menu-items">Spanish (Latin America)</li>
+                                    <li class="tech-specs__menu-items">English</li>
+                                    <li class="tech-specs__menu-items">Portuguese (Brazil)</li>
+                                    <li class="tech-specs__menu-items">French</li>
+                                </ul>
 
+                                <div class="tech-specs__menu-header">File Size</div>
 
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-            </div>
-        </div>
+                                <div class="tech-specs__menu-items">84.01 GB</div>
 
-        <!-- </div> -->
-    </section>
-    <!-- / About Slider -->
+                                <div id="ember970" class="__desktop-presentation__product-detail__details-list__23fd5 ember-view">
+                                </div><!---->
+                            </div> <!--/. pivot menu -->
+                        </div> <!--/. Card Body -->
+                    </div>  
+                </div> <!--/. Accordion Card -->   
+            </div> <!--/.Accordion wrapper-->
 
-
-    <!-- Best Selling Products -->
-    <section class="about-us-area mb-4">
-        <div class="container">
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <div class="section-heading mb-50 mt-5">--}}
-{{--                        <h5>About Our Company</h5>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-            <div class="row ">
-                <div class="col-8 ">
-                    <div>
-                        <div style="background-color: #f9f8f6">
-                            <!-- Product Image -->
-                            <img class="normal_img" src="/images/product1.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="about_txt">
-                        <div class="detail-view">
-                            <p class="text-uppercase">Sullivan Multifunction Navy Leather Watch</p>
-                            <p class="font-weight-bold">$2,889.00</p>
-                            <div class="row">
-                                <form action="#"><input type="number" class="mt-4 mb-4 ml-3 form-control" name="days" value="1" min="0" max="10"></form>
-                                <button type="button" class="btn-cart mt-4 mb-6">Add to cart</button>
-                            </div>
-                            <p>Product Details</p>
-                            <p class="font-weight-bold">Sku: <span class="text-muted">BQ2449</span></p>
-                            <p class="font-weight-bold">Case Size: <span class="text-secondary">44 mm</span></p>
-                            <p class="font-weight-bold">Movement: <span class="text-secondary">Quartz Multifunction</span></p>
-                            <p class="font-weight-bold">Platform: <span class="text-secondary text-uppercase">SULLIVAN</span></p>
-                            <p class="font-weight-bold">Strap Material: <span class="text-secondary">Leather</span></p>
-                            <p class="font-weight-bold">Water Resistance: <span class="text-secondary">5 ATM</span></p>
-                            <p>Sullivan introduced the first Fifty Fathoms watch in 1953 as the first modern diver's
-                                watch and this iconic version,water-resistant at up to 1000 feet offers an update on
-                                the classic with an added date marker and luminescent hands.
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
-
-
-            </div>
-        </div>
-    </section>
-    <!-- Best Selling Products -->
+        </div> <!-- /. Col Description-->
+    </div> <!-- /. row -->
+    
 
 @endsection
