a50ba98c0b28c48b5b232fd694bb62c450cb48bb
Rahwa Netsereab <rahwa3netsereab@gmail.com>
2020-08-02 21:29:02 -0500

    added header in patials and corrected missing steps

--- watches/resources/views/layouts/layout.blade.php
+++ watches/resources/views/layouts/layout.blade.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <!-- Title  -->
-    <title>Techwatch Home</title>
-    <!-- Favicon  -->
-    <!-- <link rel="icon" href="img/core-img/favicon.ico"> -->
-    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- Style CSS -->   
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
+@include('partials/header')
 <body>
     <!-- Header Area -->
     @include('partials/nav')
@@ -93,15 +74,3 @@
     @include('partials/footer')
      <!-- /. Footer Area -->
 
-<!-- Bootstrap core JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-
-<!-- Third party plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-<!-- Core theme JS-->
-<script src="main.js"></script>
-
-</body>
-
-</html>
