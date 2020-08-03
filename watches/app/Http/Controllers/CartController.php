e1c305270ef6d0a8e793991c69227126c13c360a
tech-soldier <yadernayapchela@gmail.com>
2020-08-03 14:37:58 -0500

    added add to cart method to cart contrller

--- watches/app/Http/Controllers/CartController.php
+++ watches/app/Http/Controllers/CartController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Watch;
 
 class CartController extends Controller
 {
-    //
+    public function addToCart($id)
+    {
+        $watch = Watch::find($id);
+
+        //getting cart out of session
+        $cart = session()->get('cart');
+
+        // checking if cart is empty and this is the first watch in the cart
+        if (!$cart) {
+
+            $cart = [
+                $id => [
+                    "watch_name" => $watch->watch_name,
+                    "quantity" => 1,
+                    "price" => $watch->price,
+                    "cover_img" => $watch->cover_img
+                ]
+            ];
+
+            session()->put('cart', $cart);
+
+
+        }
+    }
 }
