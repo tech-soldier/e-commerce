6e3e620aab090bf8478d0385fe76f31aad28eb58
tech-soldier <yadernayapchela@gmail.com>
2020-08-02 21:29:30 -0500

    added scripts and header partials

--- watches/resources/views/layouts/layout.blade.php
+++ watches/resources/views/layouts/layout.blade.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <!-- Title  -->
-    <title>Techwatch Home</title>
-    <!-- Favicon  -->
-    <!-- <link rel="icon" href="img/core-img/favicon.ico"> -->
-    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
-    <!-- Font Awesome icons (free version)-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
+    @include('partials/header')
     <!-- Header Area -->
     @include('partials/nav')
     <!-- Header Area End -->
@@ -27,20 +7,13 @@
 
     @yield('content')
 
+    <!-- end of page content -->
+
 
     <!-- Footer Area -->
     @include('partials/footer')
-     <!-- /. Footer Area -->
+    <!-- Footer Area -->
 
-<!-- Bootstrap core JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-
-<!-- Third party plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-<!-- Core theme JS-->
-<script src="main.js"></script>
-
-</body>
-
-</html>
+    <!-- Scripts Area-->
+    @include('partials/footer')
+    <!-- Scripts Area -->
