220981f99536a49b2ccc5b53b3c65ee8ed6eadd6
ainahazel <ainaramos14@gmail.com>
2020-08-04 10:13:16 -0500

    aina -- made admin folder, made partials admin folder, made header, footer, navigaiton for admin, and filled static data for watches table

---
+++ watches/resources/views/layouts/admin.blade.php
@@ -0,0 +1,18 @@
+
+@include('partialsadmin/header')
+<body>
+
+<!-- Header Area -->
+@include('partialsadmin/nav')
+<!-- Header Area End -->
+
+<!-- page content goes here -->
+
+@yield('content')
+
+<!-- end of page content -->
+
+
+<!-- Footer Area -->
+@include('partialsadmin/footer')
+<!-- Footer Area -->
---
+++ watches/resources/views/partialsadmin/nav.blade.php
@@ -0,0 +1,36 @@
+<!-- navigation --> 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Dashboard</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Watches <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Customers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Orders</a>
+      </li>
+   	  <li class="nav-item">
+        <a class="nav-link" href="#">Transactions</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Admin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Categories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Admin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Taxes</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- end of navigation -->
