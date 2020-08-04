df9318f62a24c9808bdb9ea5f8b6b2ddd6fea810
DawnBaker <deltabravo73@gmail.ca>
2020-08-04 15:02:18 -0500

    got the add to cart link and quickview links working on watchhome.blade

--- watches/resources/views/watchhome.blade.php
+++ watches/resources/views/watchhome.blade.php
@@ -114,11 +114,11 @@
                             <div class="add_quick_wrapper d-flex">
                                 <!-- Add to cart -->
                                 <div class="product_add_to_cart text-center">
-                                    <a href="#"> Add to Cart</a>
+                                    <a href="add-to-cart/{{ $watch->id }}" data-id="{{ $watch->id }}"> Add to Cart</a>
                                 </div>
                                 <!-- Quick View -->
                                 <div class="product_quick_view text-center">
-                                    <a href="#"> Quick View</a>
+                                    <a href="{{$watch->id}}/detail"> Quick View</a>
                                 </div>
                             </div>
                         </div>
@@ -126,7 +126,7 @@
                         <div class="product_description py-2">
                             <div class="watch_info_wrapper text-center ">
                                 <a class="text-center " href="#">{{$watch->watch_name}}</a>
-                                <h6 class="text-center product-price mt-2">{{$watch->price}}</h6>
+                                <h6 class="text-center product-price mt-2">${{$watch->price}}</h6>
                                 <p class="text-center mb-0">{{$watch->watch_name}}</p>
                             </div>
                         </div>
