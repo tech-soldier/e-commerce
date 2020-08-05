e672b809d8910ccf34143306c9d0d7d6919072f4
Rahwa Netsereab <rahwa3netsereab@gmail.com>
2020-08-05 12:38:23 -0500

    resoliving errors in edit page

--- watches/resources/views/admin/watches_table.blade.php
+++ watches/resources/views/admin/watches_table.blade.php
@@ -32,7 +32,7 @@
 	      <td>$ {{ $watch->price }}</td>
 	      <td>$ {{ $watch->cost }}</td>
 	      <td>{{ $watch->material }}</td>
-	      <td> <p><a href="/admin/edit/{{ $watch->watch_id }}/edit_watch" class="btn btn-primary">Edit</a></p>
+	      <td> <p><a href="/admin/edit/{{ $watch->id }}/edit_watch" class="btn btn-primary">Edit</a></p>
 	      <td><button type="button" class="btn btn-danger">Delete</button></td>
 
 
