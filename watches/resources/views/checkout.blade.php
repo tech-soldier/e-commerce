e7646ccaeb771dec25b93ebc06521e083ec5fab8
tech-soldier <yadernayapchela@gmail.com>
2020-08-05 22:06:15 -0500

    added some css to checkout form

--- watches/resources/views/checkout.blade.php
+++ watches/resources/views/checkout.blade.php
@@ -23,44 +23,44 @@
                                 <div class="form-row">
                                     <div class="col form-group">
                                         <label>First name</label>
-                                        <input type="text" class="form-control" name="first_name">
+                                        <input type="text" class="form-control bg-light" name="first_name">
                                     </div>
                                     <div class="col form-group">
                                         <label>Last name</label>
-                                        <input type="text" class="form-control" name="last_name">
+                                        <input type="text" class="form-control bg-light" name="last_name">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Address</label>
-                                    <input type="text" class="form-control" name="address">
+                                    <input type="text" class="form-control bg-light" name="address">
                                 </div>
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label>City</label>
-                                        <input type="text" class="form-control" name="city">
+                                        <input type="text" class="form-control bg-light" name="city">
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label>Country</label>
-                                        <input type="text" class="form-control" name="country">
+                                        <input type="text" class="form-control bg-light" name="country">
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div class="form-group  col-md-6">
                                         <label>Province</label>
-                                        <input type="text" class="form-control" name="province">
+                                        <input type="text" class="form-control bg-light" name="province">
                                     </div>
                                     <div class="form-group  col-md-6">
                                         <label>Postal Code</label>
-                                        <input type="text" class="form-control" name="postal_code">
+                                        <input type="text" class="form-control bg-light" name="postal_code">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Phone Number</label>
-                                    <input type="phone" class="form-control" name="phone" value="">
+                                    <input type="phone" class="form-control bg-light" name="phone" value="">
                                 </div>
                                 <div class="form-group">
                                     <label>Email Address</label>
-                                    <input type="email" class="form-control" name="email" value="{{ auth()->user()->email }}">
+                                    <input type="email" class="form-control bg-light" name="email" value="{{ auth()->user()->email }}">
                                 </div>
 
 
@@ -105,7 +105,7 @@
                                 </div>
                             </div>
                             <div class="col-md-12 mt-4">
-                                <button type="submit" class="subscribe btn btn-success btn-lg btn-block">Place Order</button>
+                                <button type="submit" class="subscribe btn btn-primary btn-lg btn-block">Place Order</button>
                             </div>
                         </div>
                     </div>
