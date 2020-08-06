83b84a5900449473ebc1cd3bcd7d87ecf53a1a76
DawnBaker <deltabravo73@gmail.ca>
2020-08-06 18:21:23 -0500

    updated route for put and update request, now just at one error left, required id field but it is not able to update, AND if i take it out of required, it gives error Creating default object from empty value

--- watches/app/Http/Controllers/Admin/WatchController.php
+++ watches/app/Http/Controllers/Admin/WatchController.php
@@ -143,7 +143,7 @@ class WatchController extends Controller
     {
 
         $valid = $request->validate([
-            'id' => 'required|integer',
+            'watch_id' => 'required|integer',
             'SKU' => 'required|integer', 
             'watch_name' => 'required|string|max:255',
             'in_stock' => 'required|integer', 
@@ -172,7 +172,7 @@ class WatchController extends Controller
         $path = $file->storeAs('storage', $cover_img);
     }
 
-        $watch = Watch::find($valid['id']);
+        $watch = Watch::find($request['watch_id']);
         $watch->SKU = $valid['SKU'];
         $watch->watch_name = $valid['watch_name'];
         $watch->in_stock = $valid['in_stock'] ?? 0;
--- watches/routes/web.php
+++ watches/routes/web.php
@@ -115,7 +115,7 @@ Route::post('admin/create/create_user', 'Admin\UserController@store');
 //edit watch table
 Route::get('/admin/edit/{id}/edit_watch', 'Admin\WatchController@edit');
 
-Route::put('/admin/edit/watches_table', 'Admin\WatchController@update');
+Route::put('/admin/edit/{id}', 'Admin\WatchController@update');
 
 Route::delete('/admin/watches_table', 'Admin\WatchController@destroy');
 
