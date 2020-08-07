648089bbfb226324acd768a988aac65aa3d8467b
AlexandrPasko <pasko2050@gmail.com>
2020-08-06 21:52:54 -0500

    update profile edit feature added

--- watches/app/Http/Controllers/UserController.php
+++ watches/app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class UserController extends Controller
 {
     /**
@@ -56,7 +58,10 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user=User::find($id);
+        $title = 'Edit Profile';
+
+        return view('/profile_edit', compact('user','title'));
     }
 
     /**
@@ -66,9 +71,55 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        // validate the record beeing updated
+        $valid = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'billing_address' => 'nullable|string|max:255',
+            'city' => 'nullable|string|max:255',
+            'province' => 'nullable|string|max:255',
+            'country' => 'nullable|string|max:255',
+            'postal_code' => 'nullable|string|max:255',
+            'email' => 'required|string|max:255',
+            'phone_number' => 'nullable|string|min:7|max:32',
+            //'category_id' => 'required|integer',
+            //'image' => 'nullable|image',            
+            'id' => 'required|integer'
+        ]);
+
+        // if image provided save the file in storage and get the name of the image
+        /*if(!empty($valid['image'])){
+            $file = $request->file('image'); // get the uploaded file
+            $image = time() . '_' . $file->getClientOriginalName(); //get the original filename
+            $path = $file->storeAs('images', $image); // save the image (link to storage added) 
+        }*/
+
+        // find the record in database
+        $user = User::find($valid['id']);
+
+        // assign new values (validated)
+        $user->first_name = $valid['first_name'];
+        $user->last_name = $valid['last_name'];
+        $user->billing_address = $valid['billing_address'];
+        /*if(!empty($image)){
+            $user->image = $image; // saving just a filename            
+        }*/
+        $user->city = $valid['city'];
+        $user->province = $valid['province'];
+        $user->country = $valid['country'];
+        $user->postal_code = $valid['postal_code'];
+        $user->email = $valid['email'];
+        $user->phone_number = $valid['phone_number'];
+
+        // if changes were made redirect to list view with success message
+        if( $user->save()){
+            return redirect('/profile')->with('success', 'Your profile has been successfully updated!');
+        }
+
+        // something went wrong, redirect to list view with error message
+        return redirect('/profile')->with('error', 'There was a problem updating your profile');        
     }
 
     /**
---
+++ watches/resources/views/profile_edit.blade.php
@@ -0,0 +1,126 @@
+
+@extends('layouts/layout')
+
+@section('content')
+
+    <div class="container page my-5" id="profile_page">
+
+        <div class="card my-3">
+            <div class="card-header bg-secondary text-light">
+                <h1 class="card-title my-2">{{ $title }}</h1>
+            </div> 
+
+            <div class="card-body">
+                <form class="form" action="/profile/edit" method="post" enctype="multipart/form-data">
+
+                    @method('PUT')
+
+                    <div class="form-group">
+                        <label for="first_name">First Name</label>
+                        <input type="text" class="form-control" name="first_name" id="first_name" value="{{ old('first_name', $user->first_name) }}" />
+
+                        @error('first_name')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div> 
+
+                    <div class="form-group">
+                        <label for="last_name">Last Name</label>
+                        <input type="text" class="form-control" name="last_name" id="last_name" value="{{ old('last_name', $user->last_name) }}" />
+
+                        @error('last_name')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>  
+
+                    <div class="form-group">
+                        <label for="billing_address">Billing Address</label>
+                        <input type="text" class="form-control" name="billing_address" id="billing_address" value="{{ old('billing_address', $user->billing_address) }}" />
+
+                        @error('billing_address')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="city">City</label>
+                        <input type="text" class="form-control" name="city" id="city" value="{{ old('city', $user->city) }}" />
+
+                        @error('city')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>                               
+
+                    <div class="form-group">
+                        <label for="province">Province</label>
+                        <input type="text" class="form-control" name="province" id="province" value="{{ old('province', $user->province) }}" />
+
+                        @error('province')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="country">Country</label>
+                        <input type="text" class="form-control" name="country" id="country" value="{{ old('country', $user->country) }}" />
+
+                        @error('country')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="postal_code">Postal Code</label>
+                        <input type="text" class="form-control" name="postal_code" id="postal_code" value="{{ old('postal_code', $user->postal_code) }}" />
+
+                        @error('postal_code')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>                
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" name="email" id="email" value="{{ old('email', $user->email) }}" />
+
+                        @error('email')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="phone_number">Phone Number</label>
+                        <input type="text" class="form-control" name="phone_number" id="phone_number" value="{{ old('phone_number', $user->phone_number) }}" />
+
+                        @error('phone_number')
+                            <span class="alert-danger">{{ $message }}</span>
+                        @enderror
+                    </div>                
+                   
+
+                    <!-- <div class="form-group">
+                                      <label for="image">user Image</label>
+                                      <input type="file" class="" name="image" id="image" value="{{ old('image', $user->image) }}" />
+                                      @error('image')
+                                          <span class="alert-danger">{{ $message }}</span>
+                                      @enderror
+                                  </div> -->              
+
+                    @csrf
+
+                    <input type="hidden" name="id" value="{{ old('id', $user->id) }}">
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+
+                </form>
+            </div>
+
+            <div class="card-footer bg-secondary text-white">
+                <p class="text-center">Your personal information is protected</p>
+            </div>
+        </div>      
+
+    </div>
+
+@endsection
