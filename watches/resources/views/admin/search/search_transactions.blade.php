013b217de2496603cbd5903d5c05c17b1050e8b8
ainahazel <ainaramos14@gmail.com>
2020-08-07 08:51:05 -0500

    aina -- finished 404 for transaction table

--- watches/resources/views/admin/search/search_transactions.blade.php
+++ watches/resources/views/admin/search/search_transactions.blade.php
@@ -11,7 +11,7 @@
 		@csrf
 	    <div class="input-group">
 	        <input type="text" class="form-control" name="query"
-	            placeholder="Search watches"> <span class="input-group-btn">
+	            placeholder="Search by Transaction ID"> <span class="input-group-btn">
 	            <button type="submit" class="btn btn-default">
 	                <span class="glyphicon glyphicon-search"></span>
 	            </button>
@@ -20,37 +20,43 @@
 	    <p><a style="color: white;" href="/admin/transactions_table"><div class="btn btn-info">Refresh Results &#x27F3;</div></a></p>
 
 	</form>
+	@if(count($transactions) > 0)
+		<!-- this is the table --> 
+		<table class="table table-striped">
+		  <thead class="thead-dark">
+		    <tr>
+		      <th scope="col">Transaction ID</th>
+		      <th scope="col">Order ID</th>
+		      <th scope="col">Transaction Code</th>
+		      <th scope="col">Response Code</th>
+		      <th scope="col">Auth Code</th>
+		      <th scope="col">Order ID</th>
+		      <th scope="col">Transaction</th>
+		      <th scope="col">Created On</th>
+		    </tr>
+		  </thead>
+		   @foreach($transactions as $transaction)
+		  <tbody>
+		    <tr>
+		      <th scope="row">{{ $transaction->id }}</th>
+		      <th>{{ $transaction->order_id }}</th>
+		      <th>{{ $transaction->transaction_code }}</th>
+		      <th>{{ $transaction->response_code }}</th>
+		      <th>{{ $transaction->auth_code }}</th>
+		      <th>{{ $transaction->order_id }}</th>
+		      <td>{{ $transaction->transaction }}</td>
+		      <td>{{ $transaction->created_at }}</td>
+		    </tr>
+		  </tbody>
+		  @endforeach
+		</table>
+		<!-- end of the table-->
+	@else
 
-<!-- this is the table --> 
-	<table class="table table-striped">
-	  <thead class="thead-dark">
-	    <tr>
-	      <th scope="col">Transaction ID</th>
-	      <th scope="col">Order ID</th>
-	      <th scope="col">Transaction Code</th>
-	      <th scope="col">Response Code</th>
-	      <th scope="col">Auth Code</th>
-	      <th scope="col">Order ID</th>
-	      <th scope="col">Transaction</th>
-	      <th scope="col">Created On</th>
-	    </tr>
-	  </thead>
-	   @foreach($transactions as $transaction)
-	  <tbody>
-	    <tr>
-	      <th scope="row">{{ $transaction->id }}</th>
-	      <th>{{ $transaction->order_id }}</th>
-	      <th>{{ $transaction->transaction_code }}</th>
-	      <th>{{ $transaction->response_code }}</th>
-	      <th>{{ $transaction->auth_code }}</th>
-	      <th>{{ $transaction->order_id }}</th>
-	      <td>{{ $transaction->transaction }}</td>
-	      <td>{{ $transaction->created_at }}</td>
-	    </tr>
-	  </tbody>
-	  @endforeach
-	</table>
-	<!-- end of the table-->
+		<h1><strong>404 Error: </strong><em>Sorry, we couldn't find what you were looking for<em></h1>
+
+
+	@endif 
 </div>
 
 @stop 
