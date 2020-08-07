8152254d5547973d041d68514ad9876ed52ec52a
ainahazel <ainaramos14@gmail.com>
2020-08-07 14:21:37 -0500

    aina -- chaged first_name to full_name in orders table as requested

--- watches/database/migrations/2020_07_30_172214_create_orders_table.php
+++ watches/database/migrations/2020_07_30_172214_create_orders_table.php
@@ -17,7 +17,7 @@ class CreateOrdersTable extends Migration
             $table->id();
             //$table->integer('watch_id')->unsigned(); 
             $table->integer('user_id')->unsigned(); 
-            $table->string('first_name', 255); 
+            $table->string('full_name', 255); 
             $table->string('email', 255); 
             $table->string('billing_address', 255); 
             $table->string('shipping_address', 255); 
