06829f553cc46b86fd2f1c97a468ed23b02f04d8
tech-soldier <yadernayapchela@gmail.com>
2020-08-06 19:33:52 -0500

    added form default values from users table

--- watches/resources/views/checkout.blade.php
+++ watches/resources/views/checkout.blade.php
@@ -29,32 +29,32 @@ use \App\Http\Controllers\CartController;
                                 <div class="form-row">
                                     <div class="col form-group">
                                         <label>First name</label>
-                                        <input type="text" class="form-control bg-light" name="first_name">
+                                        <input type="text" class="form-control bg-light" name="first_name" value="{{ auth()->user()->first_name }}">
                                     </div>
                                     <div class="col form-group">
                                         <label>Last name</label>
-                                        <input type="text" class="form-control bg-light" name="last_name">
+                                        <input type="text" class="form-control bg-light" name="last_name" value="{{ auth()->user()->last_name }}">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Address</label>
-                                    <input type="text" class="form-control bg-light" name="address">
+                                    <input type="text" class="form-control bg-light" name="address" value="{{ auth()->user()->billing_address }}">
                                 </div>
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label>City</label>
-                                        <input type="text" class="form-control bg-light" name="city">
+                                        <input type="text" class="form-control bg-light" name="city" value="{{ auth()->user()->city }}">
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label>Country</label>
-                                        <input type="text" class="form-control bg-light" name="country">
+                                        <input type="text" class="form-control bg-light" name="country" value="{{ auth()->user()->country }}">
                                     </div>
                                 </div>
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label for="exampleFormControlSelect1">Province</label>
                                         <select class="form-control province" id="exampleFormControlSelect1">
-                                            <option disabled selected> -- select an option -- </option>
+                                            <option disabled selected> -- Select Province -- </option>
                                             @foreach($taxes as $tax)
                                                 <option value="{{$tax->province}}">
                                                     {{$tax->province}}
@@ -64,12 +64,12 @@ use \App\Http\Controllers\CartController;
                                     </div>
                                     <div class="form-group  col-md-6">
                                         <label>Postal Code</label>
-                                        <input type="text" class="form-control bg-light" name="postal_code">
+                                        <input type="text" class="form-control bg-light" name="postal_code" value="{{ auth()->user()->postal_code }}">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label>Phone Number</label>
-                                    <input type="phone" class="form-control bg-light" name="phone" value="">
+                                    <input type="phone" class="form-control bg-light" name="phone" value="{{ auth()->user()->phone_number }}">
                                 </div>
                                 <div class="form-group">
                                     <label>Email Address</label>
@@ -126,7 +126,7 @@ use \App\Http\Controllers\CartController;
                                         <div class="form-group col-md-6">
                                             <label for="exampleFormControlSelect1">Province</label>
                                             <select class="form-control" id="exampleFormControlSelect1">
-                                                <option disabled selected> -- select an option -- </option>
+                                                <option disabled selected> -- Select Province -- </option>
                                                 @foreach($taxes as $tax)
                                                     <option value="{{$tax->province}}">
                                                         {{$tax->province}}
@@ -159,20 +159,20 @@ use \App\Http\Controllers\CartController;
                                     </header>
                                     <article class="card-body">
                                         <dl class="dlist-align">
-                                            <dt>Subtotal: $<span id="subtotal">{{ CartController::getCartTotal() }}</span></dt>
+                                            <dt>Subtotal: <span id="subtotal">{{ CartController::getCartTotal() }}</span></dt>
                                         </dl>
                                         <dl class="dlist-align">
-                                            <dt>GST: $<span id="gst"></span></dt>
+                                            <dt>GST: <span id="gst"></span></dt>
                                         </dl>
                                         <dl class="dlist-align">
-                                            <dt>PST:  $<span id="pst"></span></dt>
+                                            <dt>PST:  <span id="pst"></span></dt>
                                         </dl>
                                         <dl class="dlist-align">
-                                            <dt>Shipping: $<span id="shipping"></span></dt>
+                                            <dt>Shipping: <span id="shipping"></span></dt>
                                         </dl>
                                         <dl class="dlist-align">
                                             <dt>Total: </dt>
-                                            <dd class="text-right h5 b" id="total_final">$20,000</dd>
+                                            <dd class="text-right h5 b" id="total_final"></dd>
                                         </dl>
                                     </article>
                                 </div>
@@ -205,7 +205,7 @@ use \App\Http\Controllers\CartController;
                         data: {_token: '{{ csrf_token() }}', province: ele, subtotal: total},
                         dataType: "json",
                         success: function (response) {
-                            alert(response.gst);
+
                             gst.text(response.gst);
                             pst.text(response.pst);
                             shipping.text(response.shipping);
