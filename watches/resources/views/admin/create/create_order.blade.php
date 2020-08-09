dc4bca8408e1acd006219a32de426d1bc153b9ff
ainahazel <ainaramos14@gmail.com>
2020-08-09 15:03:49 -0500

    aina -- added jQuery to the orders create table for HST and PST

--- watches/resources/views/admin/create/create_order.blade.php
+++ watches/resources/views/admin/create/create_order.blade.php
@@ -59,7 +59,13 @@
         @enderror
       </div>
 
-      <div class="form-group">
+      <div class="form-group">Choose Either: 
+        <button id="pst" class="btn btn-secondary">PST</button>
+        <span id="or">OR</span>
+        <button id="hst" class="btn btn-secondary">HST</button>
+      </div>
+
+      <div class="form-group pst-div">
         <label for="GST">GST: </label>
         <input type="text" class="form-control" disabled placeholder="0.05" name="GST" id="GST" value="{{ old('GST') }}">
         @error('GST')
@@ -67,7 +73,7 @@
         @enderror
       </div>
     
-      <div class="form-group">
+      <div class="form-group pst-div">
         <label for="PST">PST: </label>
         <input type="text" class="form-control" name="PST" id="PST" value="{{ old('PST') }}">
         @error('PST')
@@ -75,9 +81,9 @@
         @enderror
       </div>
 
-      <div class="form-group">
+      <div class="form-group" id="hst-div">
         <label for="HST">HST: </label>
-        <input type="text" class="form-control" hidden name="HST" id="HST" value="{{ old('HST') }}">
+        <input type="text" class="form-control" name="HST" id="HST" value="{{ old('HST') }}">
         @error('HST')
             <span class="alert-danger">{{ $message }}</span>
         @enderror
@@ -110,5 +116,27 @@
       <p><button type="submit" class="btn btn-primary">Submit</button></p>
     </form>
 </div>
+
+<script>
+$(document).ready(function(){
+  $("#hst-div").hide();
+  $('.pst-div').hide(); 
+  $("#pst").click(function(){
+    event.preventDefault();
+    $("#hst-div").hide('slow');
+    $('.pst-div').show('slow'); 
+    $('#show').hide('slow'); 
+
+  });
+  $("#hst").click(function(){
+    event.preventDefault();
+    $("#hst-div").show('slow');
+    $('.pst-div').hide('slow'); 
+    $('#hide').hide('slow'); 
+
+  });
+});
+</script>
+
 
 @stop 
