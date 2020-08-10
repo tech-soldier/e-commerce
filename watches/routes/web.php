28b31b8336b3971cfc82583d3bb4726095817c4c
AlexandrPasko <pasko2050@gmail.com>
2020-08-09 22:23:21 -0500

    admin middleware applied to admin roures

--- watches/routes/web.php
+++ watches/routes/web.php
@@ -66,82 +66,87 @@ Route::get('/home', function(){
 	return redirect('/'); // loged in user is redirected to watchhome page
 });
 
+
 /* ----------------  ADMIN ROUTES  ------------------ */
+Route::middleware('admin')->group(function{
 
-// this is all for the admin side
+	// this is all for the admin side
 
-Route::get('/admin', 'AdminController@index');
-Route::get('/admin/watches_table', 'AdminController@watches');
-Route::get('/admin/orders_table', 'AdminController@orders');
-Route::get('/admin/users_table', 'AdminController@users');
-Route::get('/admin/admin_table', 'AdminController@admin');
-Route::get('/admin/taxes_table', 'AdminController@taxes');
-Route::get('/admin/transactions_table', 'AdminController@transactions');
-Route::get('/admin/categories_table', 'AdminController@categories');
+	Route::get('/admin', 'AdminController@index');
+	Route::get('/admin/watches_table', 'AdminController@watches');
+	Route::get('/admin/orders_table', 'AdminController@orders');
+	Route::get('/admin/users_table', 'AdminController@users');
+	Route::get('/admin/admin_table', 'AdminController@admin');
+	Route::get('/admin/taxes_table', 'AdminController@taxes');
+	Route::get('/admin/transactions_table', 'AdminController@transactions');
+	Route::get('/admin/categories_table', 'AdminController@categories');
 
-/* admin search routes ------------------------------------------*/
-Route::get('/admin/search/search_watches', 'Admin\WatchController@search');
-Route::get('/admin/search/search_users', 'Admin\UserController@search'); 
-Route::get('/admin/search/search_orders', 'Admin\OrderController@search');
-Route::get('/admin/search/search_categories', 'Admin\CategoryController@search');
-Route::get('/admin/search/search_taxes', 'Admin\TaxController@search');
-Route::get('/admin/search/search_transactions', 'Admin\TransactionController@search');
-/*----------------------------------------------------------------*/
-
-
-/* admin create files ----------------------------------------------*/
-// create watches 
-Route::get('admin/create/create_watch', 'Admin\WatchController@create');
-Route::post('admin/create/create_watch', 'Admin\WatchController@store');
-// create categories 
-Route::get('admin/create/create_category', 'Admin\CategoryController@create');
-Route::post('admin/create/create_category', 'Admin\CategoryController@store');
-// create orders 
-Route::get('admin/create/create_order', 'Admin\OrderController@create');
-Route::post('admin/create/create_order', 'Admin\OrderController@store');
-// create taxes 
-Route::get('admin/create/create_tax', 'Admin\TaxController@create');
-Route::post('admin/create/create_tax', 'Admin\TaxController@store');
-// create users COMPLETE
-Route::get('admin/create/create_user', 'Admin\UserController@create');
-Route::post('admin/create/create_user', 'Admin\UserController@store');
-/* -------------------------------------------------------------------- */
+	/* admin search routes ------------------------------------------*/
+	Route::get('/admin/search/search_watches', 'Admin\WatchController@search');
+	Route::get('/admin/search/search_users', 'Admin\UserController@search'); 
+	Route::get('/admin/search/search_orders', 'Admin\OrderController@search');
+	Route::get('/admin/search/search_categories', 'Admin\CategoryController@search');
+	Route::get('/admin/search/search_taxes', 'Admin\TaxController@search');
+	Route::get('/admin/search/search_transactions', 'Admin\TransactionController@search');
+	/*----------------------------------------------------------------*/
 
 
-/* Admin EDIT files -------------------------------------------------------- */
-
-//edit watch table
-Route::get('/admin/edit/{id}/edit_watch', 'Admin\WatchController@edit');
-Route::put('/admin/edit/edit_watch', 'Admin\WatchController@update');
-
-Route::delete('/admin/watches_table', 'Admin\WatchController@destroy');
-
-//edit order table
-Route::get('/admin/edit/{id}/edit_orders', 'Admin\OrderController@edit');
-Route::put('/admin/edit/edit_orders', 'Admin\OrderController@update');
-
-Route::delete('/admin/orders_table', 'Admin\OrderController@destroy');
-
-//edit category table
-Route::get('/admin/edit/{id}/edit_category', 'Admin\CategoryController@edit');
-Route::put('/admin/edit/edit_category', 'Admin\CategoryController@update');
-
-Route::delete('/admin/categories_table', 'Admin\CategoryController@destroy');
-
-// edit taxes table
-Route::get('/admin/edit/{id}/edit_tax', 'Admin\TaxController@edit');
-Route::put('/admin/edit/{id}', 'Admin\TaxController@update');
-
-//Route::delete('/admin/taxes_table, Admin\CategoryController@destroy');
-
-Route::delete('/admin/taxes_table', 'Admin\CategoryController@destroy');
+	/* admin create files ----------------------------------------------*/
+	// create watches 
+	Route::get('admin/create/create_watch', 'Admin\WatchController@create');
+	Route::post('admin/create/create_watch', 'Admin\WatchController@store');
+	// create categories 
+	Route::get('admin/create/create_category', 'Admin\CategoryController@create');
+	Route::post('admin/create/create_category', 'Admin\CategoryController@store');
+	// create orders 
+	Route::get('admin/create/create_order', 'Admin\OrderController@create');
+	Route::post('admin/create/create_order', 'Admin\OrderController@store');
+	// create taxes 
+	Route::get('admin/create/create_tax', 'Admin\TaxController@create');
+	Route::post('admin/create/create_tax', 'Admin\TaxController@store');
+	// create users COMPLETE
+	Route::get('admin/create/create_user', 'Admin\UserController@create');
+	Route::post('admin/create/create_user', 'Admin\UserController@store');
+	/* -------------------------------------------------------------------- */
 
 
-// edit users table
+	/* Admin EDIT files -------------------------------------------------------- */
 
-Route::get('/admin/edit/{id}/edit_users', 'Admin\UserController@edit');
+	//edit watch table
+	Route::get('/admin/edit/{id}/edit_watch', 'Admin\WatchController@edit');
+	Route::put('/admin/edit/edit_watch', 'Admin\WatchController@update');
 
-Route::post('/admin/edit/edit_users', 'Admin\UserController@update');
+	Route::delete('/admin/watches_table', 'Admin\WatchController@destroy');
 
-Route::delete('/admin/users_table', 'Admin\UserController@destroy');
+	//edit order table
+	Route::get('/admin/edit/{id}/edit_orders', 'Admin\OrderController@edit');
+	Route::put('/admin/edit/edit_orders', 'Admin\OrderController@update');
+
+	Route::delete('/admin/orders_table', 'Admin\OrderController@destroy');
+
+	//edit category table
+	Route::get('/admin/edit/{id}/edit_category', 'Admin\CategoryController@edit');
+	Route::put('/admin/edit/edit_category', 'Admin\CategoryController@update');
+
+	Route::delete('/admin/categories_table', 'Admin\CategoryController@destroy');
+
+	// edit taxes table
+	Route::get('/admin/edit/{id}/edit_tax', 'Admin\TaxController@edit');
+	Route::put('/admin/edit/{id}', 'Admin\TaxController@update');
+
+	//Route::delete('/admin/taxes_table, Admin\CategoryController@destroy');
+
+	Route::delete('/admin/taxes_table', 'Admin\CategoryController@destroy');
+
+
+	// edit users table
+
+	Route::get('/admin/edit/{id}/edit_users', 'Admin\UserController@edit');
+
+	Route::post('/admin/edit/edit_users', 'Admin\UserController@update');
+
+	Route::delete('/admin/users_table', 'Admin\UserController@destroy');
+
+});
+
 
