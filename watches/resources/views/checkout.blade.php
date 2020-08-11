f2c71dcd6c6e7228efc949ae78e8f81eebea009e
tech-soldier <yadernayapchela@gmail.com>
2020-08-11 17:49:23 -0500

    added code to display shipping address fields same as billing ones if checkbox is selected

--- watches/resources/views/checkout.blade.php
+++ watches/resources/views/checkout.blade.php
@@ -37,18 +37,18 @@
                         <article class="card-body">
                             <div class="form-row">
                                 <div class="form-group">
-                                    <input type="hidden" name="user_id" disabled class="form-control" id="user_id" value="{{ auth()->user()->id }}">
+                                    <input type="hidden" name="user_id" disabled class="form-control" id="user_id" value="{{ $user['id'] }}">
                                 </div>
                                 <div class="col form-group">
                                     <label>First name</label>
-                                    <input type="text" class="form-control bg-light" name="first_name" value="{{ auth()->user()->first_name }}">
+                                    <input type="text" class="form-control bg-light" name="first_name" id="first_name" value="{{ $user['first_name'] }}">
                                     @error('id')
                                     <span class="alert-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="col form-group">
                                     <label>Last name</label>
-                                    <input type="text" class="form-control bg-light" name="last_name" value="{{ auth()->user()->last_name }}">
+                                    <input type="text" class="form-control bg-light" name="last_name" value="{{ $user['last_name'] }}">
                                     @error('last_name')
                                     <span class="alert-danger">{{ $message }}</span>
                                     @enderror
@@ -56,7 +56,7 @@
                             </div>
                             <div class="form-group">
                                 <label>Address</label>
-                                <input type="text" class="form-control bg-light" name="billing_address" value="{{ auth()->user()->billing_address }}">
+                                <input type="text" class="form-control bg-light" name="billing_address" id="billing_address" value="{{ $user['billing_address'] }}">
                                 @error('billing_address')
                                 <span class="alert-danger">{{ $message }}</span>
                                 @enderror
@@ -64,14 +64,14 @@
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label>City</label>
-                                    <input type="text" class="form-control bg-light" name="city" id="city" value="{{ auth()->user()->city }}">
+                                    <input type="text" class="form-control bg-light" name="city" id="city" value="{{ $user['city'] }}">
                                     @error('city')
                                     <span class="alert-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label>Country</label>
-                                    <input type="text" class="form-control bg-light" name="country" value="{{ auth()->user()->country }}">
+                                    <input type="text" class="form-control bg-light" name="country" id="country" value="{{ $user['country'] }}">
                                     @error('country')
                                     <span class="alert-danger">{{ $message }}</span>
                                     @enderror
@@ -80,7 +80,7 @@
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="exampleFormControlSelect1">Province</label>
-                                    <select class="form-control province" id="exampleFormControlSelect1" name="province">
+                                    <select class="form-control province" id="province" name="province">
                                         <option disabled selected> -- Select Province -- </option>
                                         @foreach($taxes as $tax)
                                             <option value="{{$tax->province}}">
@@ -94,7 +94,7 @@
                                 </div>
                                 <div class="form-group  col-md-6">
                                     <label>Postal Code</label>
-                                    <input type="text" class="form-control bg-light" name="postal_code" value="{{ auth()->user()->postal_code }}">
+                                    <input type="text" class="form-control bg-light" name="postal_code" id="postal_code" value="{{ $user['postal_code'] }}">
                                     @error('postal_code')
                                     <span class="alert-danger">{{ $message }}</span>
                                     @enderror
@@ -102,14 +102,14 @@
                             </div>
                             <div class="form-group">
                                 <label>Phone Number</label>
-                                <input type="phone" class="form-control bg-light" name="phone_number" value="{{ auth()->user()->phone_number }}">
+                                <input type="phone" class="form-control bg-light" name="phone_number" value="{{ $user['phone_number'] }}">
                                 @error('phone_number')
                                 <span class="alert-danger">{{ $message }}</span>
                                 @enderror
                             </div>
                             <div class="form-group">
                                 <label>Email Address</label>
-                                <input type="email" class="form-control bg-light" name="email" value="{{ auth()->user()->email }}">
+                                <input type="email" class="form-control bg-light" name="email" value="{{ $user['email'] }}">
                                 @error('email')
                                 <span class="alert-danger">{{ $message }}</span>
                                 @enderror
@@ -167,66 +167,66 @@
                         </article>
                     </div>  <!-- /.card -->
 
-                        <div class="card mt-3">
-                            <header class="card-header bg-secondary text-white">
-                                <h4 class="card-title mt-2">Shipping Details</h4>
-                            </header>
-                            <article class="card-body">
+                    <div class="card mt-3">
+                        <header class="card-header bg-secondary text-white">
+                            <h4 class="card-title mt-2">Shipping Details</h4>
+                        </header>
+                        <article class="card-body">
 
-                                <div class="form-group">
-                                    <label>Address</label>
-                                    <input type="text" class="form-control bg-light" name="shipping_address" value="{{ auth()->user()->billing_address }}">
-                                    @error('shipping_address')
+                            <div class="form-group">
+                                <label>Address</label>
+                                <input type="text" class="form-control bg-light" name="shipping_address" id="shipping_address" value="{{ old('shipping_address') }}">
+                                @error('shipping_address')
+                                <span class="alert-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label>City</label>
+                                    <input type="text" class="form-control bg-light" name="shipping_city" id="shipping_city" value="{{ old('shipping_city') }}">
+                                    @error('shipping_city')
                                     <span class="alert-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label>City</label>
-                                        <input type="text" class="form-control bg-light" name="shipping_city" value="{{ auth()->user()->city }}">
-                                        @error('shipping_city')
-                                        <span class="alert-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label>Country</label>
-                                        <input type="text" class="form-control bg-light" name="shipping_country" value="{{ auth()->user()->country }}">
-                                        @error('shipping_country')
-                                        <span class="alert-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
+                                <div class="form-group col-md-6">
+                                    <label>Country</label>
+                                    <input type="text" class="form-control bg-light" name="shipping_country" id="shipping_country" value="{{ old('country') }}">
+                                    @error('shipping_country')
+                                    <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="exampleFormControlSelect1">Province</label>
-                                        <select class="form-control" id="exampleFormControlSelect1" name="shipping_province">
-                                            <option disabled selected> -- Select Province -- </option>
-                                            @foreach($taxes as $tax)
-                                                <option value="{{$tax->province}}">
-                                                    {{$tax->province}}
-                                                </option>
-                                            @endforeach
-                                        </select>
-                                        @error('shipping_province')
-                                        <span class="alert-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="form-group  col-md-6">
-                                        <label>Postal Code</label>
-                                        <input type="text" class="form-control bg-light" name="shipping_postal_code" value="{{ auth()->user()->postal_code }}">
-                                        @error('shipping_postal_code')
-                                        <span class="alert-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="exampleFormControlSelect1">Province</label>
+                                    <select class="form-control" id="shipping_province" name="shipping_province">
+                                        <option disabled selected> -- Select Province -- </option>
+                                        @foreach($taxes as $tax)
+                                            <option value="{{$tax->province}}">
+                                                {{$tax->province}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    @error('shipping_province')
+                                    <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
+                                <div class="form-group  col-md-6">
+                                    <label>Postal Code</label>
+                                    <input type="text" class="form-control bg-light" name="shipping_postal_code" id="shipping_postal_code" value="{{ old('shipping_postal_code') }}">
+                                    @error('shipping_postal_code')
+                                    <span class="alert-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
 
-{{--                                    <div class="form-check">--}}
-{{--                                        <input type="checkbox" class="form-check-input" id="materialUnchecked">--}}
-{{--                                        <label class="form-check-label" for="materialUnchecked">Same as Billing Address</label>--}}
-{{--                                    </div>--}}
+                            <div class="form-check">
+                                <input type="checkbox" name="check-address" class="form-check-input" id="check-address">
+                                <label class="form-check-label" for="materialUnchecked">Same as Billing Address</label>
+                            </div>
 
-                            </article>
-                        </div>
+                        </article>
+                    </div>
                 </div>
 
 
@@ -296,6 +296,21 @@
 
                 }
             });
+        });
+
+        $('#check-address').change(function() {
+
+
+            if ($('#check-address').is(':checked')) {
+
+                $('#shipping_address').val($("#billing_address").val());
+                $('#shipping_city').val($("#city").val());
+                $('#shipping_country').val($("#country").val());
+                $('#shipping_postal_code').val($("#postal_code").val());
+                $('#shipping_province').val($("#province").val());
+
+            }
+
         });
 
 
